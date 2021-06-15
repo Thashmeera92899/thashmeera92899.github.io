29040de0f328544ed13659e6956b688ba881846f
Thashmeera92899 <92899@fhss.sjp.ac.lk>
2021-06-15 22:34:56 +0530

    Updates

---
+++ precache-manifest.e6d1914f509c4f217e8e49e81eb0adb1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15b50068a6b9c6b403c083c535ea58e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bffb73d59ac3c849e5d3",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bffb73d59ac3c849e5d3",
+    "url": "/static/js/main.39c32cdf.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "cf07afa38a68e654a5062fef65752755",
+    "url": "/static/media/resume.cf07afa3.pdf"
+  },
+  {
+    "revision": "9b09913061412cf3a0edc31ab026fc03",
+    "url": "/static/media/thash.9b099130.jpg"
+  }
+]);
